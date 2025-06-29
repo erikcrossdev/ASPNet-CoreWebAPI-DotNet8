99ae930d403b2e2661a38c0de8f0a10d8cd6cb35
ErikCruz <erikcruzdev@gmail.com>
2025-06-29 16:39:32 -0300

    Add Controller, and set swagger UI work

---
+++ api/Controllers/StockController.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/stock")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly ApplicationDBContext _context;
+        public StockController(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+        [HttpGet] //get = read
+        public IActionResult GetAll()
+        {
+            var stocks = _context.Stocks.ToList(); //needs .ToList to convert to a list
+            return Ok(stocks); //returns 200 OK with the list of stocks           
+        }
+
+        [HttpGet("{id}")] //get by id, .Net will use model binding to extract the string into an int 
+        public IActionResult GetById([FromRoute] int id)
+        {
+            var stock = _context.Stocks.Find(id);
+            if (stock == null)
+            {
+                return NotFound(); //returns 404 Not Found if stock is not found
+            }
+            return Ok(stock); //returns 200 OK with the stock
+        }
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -3,9 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args); //control dependency injection
 
+builder.Services.AddControllers(); //add controllers to the service collection
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+//builder.Services.AddOpenApi(); //removed to use swagger instead
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<ApplicationDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
@@ -15,8 +19,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    //app.MapOpenApi();
+    app.UseSwagger();
+   app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers(); //map controllers to the app
 app.Run();
