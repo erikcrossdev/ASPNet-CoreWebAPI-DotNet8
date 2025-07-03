f620fcec1eb2c5b91b7e2650a38b080d886e563a
ErikCruz <erikcruzdev@gmail.com>
2025-07-03 15:57:29 -0300

    Create a POST Stock

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.DTOs.Stock;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,7 +21,7 @@ namespace api.Controllers
         [HttpGet] //get = read
         public IActionResult GetAll()
         {
-            var stocks = _context.Stocks.ToList().Select( s => s.ToStockDTO()); //use the mapper to convert the stock model to stock DTO
+            var stocks = _context.Stocks.ToList().Select(s => s.ToStockDTO()); //use the mapper to convert the stock model to stock DTO
             return Ok(stocks); //returns 200 OK with the list of stocks           
         }
 
@@ -33,6 +34,16 @@ namespace api.Controllers
                 return NotFound(); //returns 404 Not Found if stock is not found
             }
             return Ok(stock.ToStockDTO()); //returns 200 OK with the stock
+        }
+
+        [HttpPost] //post = create
+        public IActionResult Create([FromBody] CreateStockRequestDTO stockDTO)
+        {
+            var stockModel = stockDTO.ToStockFromCreateDTO(); //use the mapper to convert the stock DTO to stock model
+            _context.Stocks.Add(stockModel); //add the stock model to the context
+            _context.SaveChanges(); //save changes to the database
+
+            return CreatedAtAction(nameof(GetById), new { id = stockModel.Id }, stockModel.ToStockDTO()); //returns 201 Created with the stock DTO and the location of the created resource
         }
     }
 }
